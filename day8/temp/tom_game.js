224388030b53acc8ff316a3af0f387c9371a4704
Tom Joseph <tom.joseph@detroitlabs.com>
2015-03-04 17:09:30 -0500

    dungeon object, filling object arrays

--- day8/temp/tom_game.js
+++ day8/temp/tom_game.js
@@ -1,5 +1,28 @@
 //var sg = require('sget');
 
+function game() {
+	this.Name = "Adventure";
+}
+	game.prototype.startGame = function() {
+		console.log("The game is afoot!");
+		var dungeon = new dungeon();
+		
+		console.log(room1.menuItem[0].stdDescription);
+		room1.menuItem.push(new menuOption(1, "blah", "hlab", 0));
+		console.log(room1.menuItem[1].stdDescription);
+		
+		game.prototype.exitGame();
+	};
+
+	game.prototype.exitGame = function(){
+		process.exit(0);
+	};
+	game.prototype.printRoom = function() {};
+	game.prototype.getChoice = function() {};
+	game.prototype.exitGame = function() {
+		process.exit(0);
+	};
+
 function player(inventory, startRoom) {
 	this.inventory = inventory;
 	this.currentRoom = startRoom;
@@ -25,12 +48,19 @@ function roomItem(name, taken) {
 	this.roomID = 0;
 }
 
-function menuItem() {
-	this.menuID = 0;
-	this.stdDescription = " ";
-	this.altDescription = " ";
-	this.roomID = 0;		//Not sure if this will be used
+function menuOption(id, desc, altDesc, roomID) {
+	this.menuID = id;
+	this.stdDescription = desc;
+	this.altDescription = altDesc;
+	this.roomID = roomID;				//Not sure if this will be used
 }
+
+function dungeon() {
+	this.rooms = [new room()];
+}
+	dungeon.prototype.populateDungeon = function () {
+
+	};
 
 function room() {
 	this.roomID = 0;
@@ -40,35 +70,15 @@ function room() {
 	this.numberItems = 2;
 	this.roomDelimeter = "----------";
 
-	this.menuOption = [1];
-	this.roomItem = [1];
-	
-	
-
-	
-}
-
-function game() {
-	this.Name = "Adventure";
-}
-	game.prototype.startGame = function() {
-			console.log("The game is afoot!");
-			game.prototype.exitGame();
-	};
-
-	game.prototype.exitGame = function(){
-		process.exit(0);
-	};
-	game.prototype.printRoom = function() {};
-	game.prototype.getChoice = function() {};
-
-function exitGame() {
-	process.exit(0);
+	this.menuItem = [new menuOption(0, "test", "tset", 1)];
+	this.roomItem = [new roomItem("ball", false)];	
 }
 
 function clearScreen() {
 	process.stdout.write('\033c');
 }
 
+/* --------------- CODE TO ACTUALLY RUN THE GAME -------------- */
 var adventureGame = new game();
+clearScreen();
 adventureGame.startGame();
